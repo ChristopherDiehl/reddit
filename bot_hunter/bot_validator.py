2d5f647b111734303445387763627cd6e95b6346
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-06-27 18:22:32 -0400

    going through posts next

--- bot_hunter/bot_validator.py
+++ bot_hunter/bot_validator.py
@@ -1,5 +1,9 @@
 #template from: /u/GoldenSights
-#bot to find all the other bots
+#bot to determine if bot is a bot
+#wanted to evaluate average response time, but after investigation realized there was too much variation.
+#I believe this variation was due to the irregularities associated with redidt. Ie. tons of post one day, dead the next
+#When there are no posts, autotldr and other response bots no longer need to post as frequently
+
 import praw
 import time
 import sqlite3
@@ -9,6 +13,7 @@ import requests
 import traceback
 import difflib 
 from socket import timeout
+import datetime
 
 '''USER CONFIGURATION'''
 
@@ -30,9 +35,7 @@ try:
 except ImportError:
     pass
 MAXCOMMENTS = 10
-#This is how many posts you want to retrieve all at once. PRAW can download 100 at a time.
 WAIT = 30
-#This is how many seconds you will wait between cycles. The bot is completely inactive during this time.
 
 sql = sqlite3.connect('sql.db')
 print('Loaded SQL Database')
@@ -51,41 +54,75 @@ def validate():
    try:
 
       for potentialBot in cur.execute('SELECT * FROM potentialBots'):
-      
+
          print('Validating '+potentialBot[0])
+
          x = 0
          totalSequentialDiff = 0
+         numOfHyperlinksInBaseComment = 0
+         sizeOfCommonBlock = 0
+         aBlockStart = 0
+         commonString = ""
+         hyperLinkScore = 10
+         commonStringScore = 0
+
+         comments = r.get_redditor(potentialBot[0]).get
+
          try:
+
             for comment in r.get_redditor(potentialBot[0]).get_comments(limit=MAXCOMMENTS):
                print(comment)
+               print(dir(r.get_redditor(potentialBot[0])))
                if x == 0:
                   baseComment = comment
+                  numOfHyperlinksInBaseComment = baseComment.body.count("](http")
+
+                  x +=1
+
+               elif x == 1:
+                  seq=difflib.SequenceMatcher(None, baseComment.body,comment.body)
+                  totalSequentialDiff +=(seq.ratio())
+
+
+                  if baseComment.body.count("](http") == numOfHyperlinksInBaseComment:
+                     hyperLinkScore += 10
+
+
+                  for block in seq.get_matching_blocks():
+                     if block[2] > sizeOfCommonBlock:
+                        sizeOfCommonBlock = block[2]
+                        end = int(block[0]+block[2])
+                        commonString = comment.body[block[0]:end:]
+
                   x+=1
-                  continue
 
-               seq=difflib.SequenceMatcher(None, baseComment.body,comment.body)
-               totalSequentialDiff +=(seq.ratio())
+               else: 
+                  seq=difflib.SequenceMatcher(None, baseComment.body,comment.body)
+                  totalSequentialDiff +=(seq.ratio())
+                  if baseComment.body.count("](http") == numOfHyperlinksInBaseComment:
+                     hyperLinkScore += 10
 
-
-               for block in seq.get_matching_blocks():
-                  print("block: ")
-                  print(block)
-                  print ("a[%d] and b[%d] match for %d elements" % block)
-
-               print (comment.body)
+                  if comment.body.find(commonString) != -1:
+                     commonStringScore += 12.5
+            
+            x = 0
+            totalSequentialDiff *= 10
+            print("hyperLinkScore: " +str(hyperLinkScore))
+            print("totalSequentialDiff: "+str(totalSequentialDiff))
+            print(commonString)
+            print('commonStringScore: '+str(commonStringScore))
+            commonStringScore = 0
 
          except praw.errors.NotFound:
             print('user was banned')
             pass
 
-         totalSequentialDiff *= 10
-         print(totalSequentialDiff)
-         break
 
 
    except AttributeError:
       print('AttributeError')
       pass
+
 
 
 
