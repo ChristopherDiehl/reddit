ddab6a78344b020a804fa3a2e7e1fc338c90842d
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-06-29 16:11:53 -0400

    Finished blog post bot

--- bot_hunter/blog_post.py
+++ bot_hunter/blog_post.py
@@ -1,27 +1,20 @@
 #template from: /u/GoldenSights
-#copy of autotldr
-#need to have installed praw,sumy
 import praw
 import time
 import traceback
-import requests
 from socket import timeout
 from bs4 import BeautifulSoup
-import sumy.summarizers.lsa as lsa_module
-from sumy.summarizers.lsa import LsaSummarizer
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.utils import get_stop_words
 import io
 
-MAX_SENTENCES = 2
 APP_ID = ""
 APP_SECRET = ""
 APP_URI = ""
-USERAGENT = "An autotldr copy by /u/ShaddiestTerrapin57"
+USERAGENT = "An example bot by /u/ShaddiestTerrapin57"
 SUBREDDIT = "worldnews"
-MAXPOSTS = 30
-WAIT = 160
-START_MESSAGE = "I am a bot. I was able to reduce article to 4 sentences: \n"
+MAXPOSTS = 2
+WAIT = 30
+START_MESSAGE = "I am a bot. Here is the content: \n"
+
 #a bot.py file where you list variables
 try:
 	import bot
@@ -29,6 +22,7 @@ try:
 	APP_SECRET = bot.APP_SECRET
 	APP_URI = bot.APP_URI
 	USERAGENT = bot.USERAGENT
+
 except ImportError:
 	pass
 
@@ -41,40 +35,34 @@ posts = []
 posts += subreddit.get_new(limit=MAXPOSTS)
 
 
+
 def scanSubreddit():
-	try:
+	
+	try:	
 		x = 0
 		for post in posts:
-			#url = post.url
-			print('post ['+str(x)+']')
-			url = 'http://www.bbc.com/news/world-europe-36658187'
-
+			url = post.url
 			if str(url).find('https://www.reddit.com') == -1:
 				webpage = requests.get(url)
-				soup = BeautifulSoup(webpage.content,'html5lib')			
+				soup = BeautifulSoup(webpage.content,'html5lib')	
 				parsedArticle = ''
 
+				print('url: '+url)
 				for p_tag in soup.find_all('p'):
 					parsedArticle += str(p_tag.get_text())
 
-				print(parsedArticle)
-				print('\nvs\n')
+				print(parsedArticle +'\n\n')
 
-				print('parsedArticle :' +parsedArticle)
-				post_comment(post,parsedArticle)
+			parsedArticle = ""
+			#post_comment(post,parsedArticle)
 
 	except AttributeError:
-		print('AttributeError')
+		print('AttributeError: ')
+		traceback.print_exc()
 		pass
 
-def local_summarize(articleToSummarize) :
-
-	summarizer = LsaSummarizer()
-	summarizer.stopwords = get_stop_words('ENGLISH')
-	return summarizer(articleToSummarize, MAX_SENTENCES)
-
 def post_comment(post, parsedArticle):
-	r.add_comment(START_MESSAGE + parsedArticle)
+	post.add_comment(START_MESSAGE + parsedArticle)
 	print('Comment posted: '+ parsedArticle)
 
 
